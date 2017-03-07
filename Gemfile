cbf639e18f10424fa421ec2991e9170e2490cd4e
Yanny Castrillon <yannyandres117@gmail.com>
2017-03-07 14:06:20 -0800

    add several relationships to the models. Model Slide -> acts_as_list and Model Category -> acts_as_tree. Incorporate Scopes to the actual slide model. Create the parent_id column in the category table to be able to manage the act_as_tree relationship between categories.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
-
+gem 'acts_as_tree'
+gem 'acts_as_list'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.6'
 # Use postgresql as the database for Active Record
@@ -44,4 +45,3 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
-
---
+++ db/photos_data.sql
@@ -0,0 +1,18 @@
+insert into categories (id, name, created_at, updated_at) values (1,'All',now(),now());
+insert into categories (id, name, created_at, updated_at) values (2,'People',now(),now());
+insert into categories (id, name, created_at, updated_at) values (3,'Animals',now(),now());
+insert into categories (id, name, created_at, updated_at) values (4,'Places',now(),now());
+insert into categories (id, name, created_at, updated_at) values (5,'Things',now(),now());
+insert into categories (id, name, created_at, updated_at) values (6,'Friends',now(),now());
+insert into categories (id, name, created_at, updated_at) values (7,'Family',now(),now());
+
+insert into categories_photos (category_id,photo_id)  values (4,1);
+insert into categories_photos (category_id,photo_id)  values (3,2);
+insert into categories_photos (category_id,photo_id)  values (3,3);
+insert into categories_photos (category_id,photo_id)  values (4,4);
+insert into categories_photos (category_id,photo_id)  values (5,5);
+insert into categories_photos (category_id,photo_id)  values (3,6);
+insert into categories_photos (category_id,photo_id)  values (2,7);
+insert into categories_photos (category_id,photo_id)  values (4,8);
+insert into categories_photos (category_id,photo_id)  values (4,9);
+insert into categories_photos (category_id,photo_id)  values (4,7);
